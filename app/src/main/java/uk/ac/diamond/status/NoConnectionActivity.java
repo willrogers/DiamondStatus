9cb3b038e0121280fcebd04d9c4f54e21809314d
Will Rogers <wahrogers@gmail.com>
2014-12-21 14:53:34 +0000

    Remove dead code and tidy NoConnectionActivity.

--- app/src/main/java/uk/ac/diamond/status/NoConnectionActivity.java
+++ app/src/main/java/uk/ac/diamond/status/NoConnectionActivity.java
@@ -4,9 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,21 +12,13 @@ public class NoConnectionActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         // Set up the action bar
         ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
         actionBar.setTitle(R.string.app_title);
-        actionBar.setDisplayShowTitleEnabled(true);
 
         setContentView(R.layout.activity_no_connection);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
     public void refresh(View view) {
@@ -42,24 +31,4 @@ public class NoConnectionActivity extends Activity {
         startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-            case R.id.action_refresh:
-                refresh();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
